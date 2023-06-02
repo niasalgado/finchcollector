e130138bb0574e2dd149a7d08a63f9c01a3a2def
niasalgado <salgado.nia@gmail.com>
2023-06-02 19:10:31 -0400

    creates Feeding model and makes migrations

--- main_app/models.py
+++ main_app/models.py
@@ -13,3 +13,23 @@ class Finch(models.Model):
     
     def get_absolute_url(self):
         return reverse('details', kwargs={'finch_id': self.id})
+   
+class Feeding(models.Model):
+    date = models.DateField()
+    meal = models.CharField(max_length=1)
+
+    MEALS = (
+        ('B', 'Breakfast'),
+        ('L', 'Lunch'),
+        ('D', 'Dinner'),
+    )
+    date = models.DateField('feeding date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.get_meal_display()} on {self.date}'
